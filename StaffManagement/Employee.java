d2292f00559131dc42e271705bd90226c991c071
MRIT03 <62628856+MRIT03@users.noreply.github.com>
2022-02-21 18:23:48 +0200

    Second Commit
    Second Commit

---
+++ StaffManagement/Employee.java
@@ -0,0 +1,77 @@
+public class Employee{
+	String firstName, lastName;
+	int age;
+	double salary;
+
+	Employee(String first, String last, int old, double money){
+		firstName = first;
+		lastName = last;
+		age = old;
+		salary = money;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
